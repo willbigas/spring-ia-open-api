4ab1cdc51f3a72fb80bd4879a72555a80419a511
William Bigas Mauro <ice_wmauro@uolinc.com>
2026-01-02 15:21:36 -0300

    commit inicial

---
+++ src/main/java/br/com/willbigas/Startup.java
@@ -0,0 +1,13 @@
+package br.com.willbigas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
